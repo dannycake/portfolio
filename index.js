8ecc80be7715e281e7c6006543fee07aa9a87e36
vanity <danny@danny.ink>
2023-10-22 16:54:20 -0400

    redirect to image host for 404s

--- index.js
+++ index.js
@@ -82,6 +82,10 @@ app.get('/', (req, resp) => {
     );
 });
 
+app.get('*', (req, resp) => {
+    resp.redirect(`https://s.danny.ink${req.url}`);
+})
+
 app.get('/api/activity', express.json(), async (_req, resp) => {
     return resp.send(currentActivity);
 });
